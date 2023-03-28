148dbaa2bf90d95501cef363bc85d5e4ec3aa7f2
Divyang Kanani <divyang.k@simformsolutions.com>
2023-03-28 11:25:22 +0530

    Home File Created.....

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: const Scaffold(
+        backgroundColor: Colors.white,
+        body: Text('Hello DU'),
+      ),
+    );
+  }
+}
